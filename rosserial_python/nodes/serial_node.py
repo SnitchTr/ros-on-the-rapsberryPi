dbc7f251d0ebe239e60f984f24f3b718e414bdcb
ArnauOrtega <62354394+ArnauOrtega@users.noreply.github.com>
2020-09-01 13:24:16 +0200

    Update serial_node.py

--- rosserial_python/nodes/serial_node.py
+++ rosserial_python/nodes/serial_node.py
@@ -44,11 +44,10 @@ import multiprocessing
 import sys
 
 if __name__=="__main__":
-
     rospy.init_node("serial_node")
     rospy.loginfo("ROS Serial Python Node")
 
-    port_name = rospy.get_param('~port','/dev/ttyUSB0')
+    port_name = rospy.get_param('~port','/dev/ttyACM0')
     baud = int(rospy.get_param('~baud','57600'))
 
     # for systems where pyserial yields errors in the fcntl.ioctl(self.fd, TIOCMBIS, \
@@ -93,6 +92,11 @@ if __name__=="__main__":
                 sleep(1.0)
                 continue
             except OSError:
+                sleep(1.0)
+                continue
+            except:
+                rospy.logwarn("Unexpected Error: %s", sys.exc_info()[0])
+                client.port.close()
                 sleep(1.0)
                 continue
 
